106abe6775adf9dbffcaf361a52de613ed0f46ff
gamp300 <gamp300@gmail.com>
2024-02-20 08:17:53 -0500

    React 03-Image-finder Three

--- src/components/Styles.Styled.js
+++ src/components/Styles.Styled.js
@@ -40,6 +40,23 @@ export const Li = styled.li`
   margin: 20px;
 `;
 
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+`;
 /* 
 
 @media screen and (min-width: 900px) {
